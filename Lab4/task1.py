45bb3622e831059e67a811f570e3dec1ced35e72
joelbill <joel.bill94@gmail.com>
2018-10-15 15:02:38 +0200

    re-inserted the code for lab4

--- Lab4/task1.py
+++ Lab4/task1.py
@@ -87,11 +87,11 @@ def find_pairs(formatted_corpus):
             id_n = word['id']
             new_sentence[id_n] = word
         sentences.append(new_sentence)
+
     pairs = {}
     for sentence in sentences:
         for word_id in sentence:
             word = sentence[word_id]
-            print(word)
             if '-' not in word_id and word['deprel'] == SUBJ:
                 verb_key = word['head']
                 verb = str.lower(sentence[verb_key]['form'])
@@ -106,10 +106,13 @@ def find_pairs(formatted_corpus):
     sorted_pairs = sorted(pairs.items(), key=operator.itemgetter(1), reverse=True)
     print('Number of pairs in corpus: ' + str(counter))
     print('Most frequent pairs: ')
-    for i in range(0, 200):
-        print(sorted_pairs[i])
+    for i in range(0, 5):
+        try:
+            print(sorted_pairs[i])
+        except:
+            print('No more pairs')
 
-def find_tripples(formatted_corpus):
+def find_triplesWrong(formatted_corpus):
     counter = 0
 
     sentences = []
@@ -120,87 +123,153 @@ def find_tripples(formatted_corpus):
             id_n = word['id']
             new_sentence[id_n] = word
         sentences.append(new_sentence)
-    tripples = {}
+
+    triples = {}
     for sentence in sentences:
-        id_list =[]
-        tmp_sentences = []
-        tmp_sentence = {}
-        tmp_tripple = {}
+        print('new sentence')
+        tmp_obj = {}
+        tmp_subj = {}
         for word_id in sentence:
             word = sentence[word_id]
-
+            # print(word)
             if '-' not in word_id and (word['deprel'] == SUBJ or word['deprel'] == OBJ):
-
+                # print('found ' + word['deprel'])
                 key = word['deprel']
-                tmp_sentence[word_id] = word
-                tmp_sentences.append(tmp_sentence)
-                tmp_tripple[key] = tmp_sentence
-                print(tmp_tripple[key][word_id]['head'])
-                """
-                verb_key = word['head']
-                verb = str.lower(sentence[verb_key]['form'])
-                subject = str.lower(word['form'])
-                counter += 1
-                tripple = (subject, verb)
-                if tripple in tripples:
-                    tripples[tripple] += 1
+                if key == SUBJ:
+                    tmp_subj[word_id] = word
                 else:
-                    tripples[tripple] = 1
-                """
+                    tmp_obj[word_id] = word
+                if tmp_subj is not None and tmp_obj is not None:
+                    for subj_id in tmp_subj:
+                        for obj_id in tmp_obj:
+                            if tmp_subj[subj_id]['head'] == tmp_obj[obj_id]['head']:
+                                verb_key = word['head']
+                                verb = str.lower(sentence[verb_key]['form'])
+                                counter += 1
+                                triple = (tmp_subj[subj_id]['form'], verb, tmp_obj[obj_id]['form'])
+                                if triple in triples:
+                                    triples[triple] += 1
+                                else:
+                                    triples[triple] = 1
 
-                if id_list != None and tmp_tripple[SUBJ] != None and tmp_tripple[OBJ] != None:
-                    for id in id_list:
-                        if tmp_tripple[key][word_id]['head'] == tmp_tripple[OBJ][id]['head']:
-                            print(word)
+                """if tmp_subj is not None and tmp_obj is not None and tmp_subj['head'] == tmp_obj['head']:
+                    verb_key = word['head']
+                    verb = str.lower(sentence[verb_key]['form'])
+                    counter += 1
+                    triple = (tmp_subj['form'], verb, tmp_obj['form'])
+                    # print('This is the triple: ')
+                    # print(triple)
+                    if triple in triples:
+                        triples[triple] += 1
+                    else:
+                        triples[triple] = 1
+    """
+    sorted_triples = sorted(triples.items(), key=operator.itemgetter(1), reverse=True)
+    print('Number of triples in corpus: ' + str(counter))
+    print('Most frequent triples: ')
+    for i in range(0, 5):
+        print(sorted_triples[i])
+
+def find_triples(formatted_corpus):
+    counter = 0
+
+    sentences = []
+    # Convert sentence lists to dicts
+    for sentence in formatted_corpus:
+        new_sentence = {}
+        for word in sentence:
+            id_n = word['id']
+            new_sentence[id_n] = word
+        sentences.append(new_sentence)
+
+    triples = {}
+    for sentence in sentences:
+        id_subj = []
+        id_obj = []
+        tmp_subj = {}
+        tmp_obj = {}
+        for word_id in sentence:
+            word = sentence[word_id]
+            if '-' not in word_id and (word['deprel'] == SUBJ or word['deprel'] == OBJ):
+                key = word['deprel']
+
+                if id_obj is not None and id_subj is not None:
+
+                    if key == SUBJ:
+                        tmp_subj[word_id] = word
+                        for id in id_obj:
+                            if tmp_subj[word_id]['head'] == tmp_obj[id]['head']:
+                                counter += 1
+                                verb_key = word['head']
+                                verb = (str.lower(sentence[verb_key]['form']))
+                                subj = (str.lower(tmp_subj[word_id]['form']))
+                                obj = (str.lower(tmp_obj[id]['form']))
+                                triple = (subj, verb, obj)
+                                if triple in triples:
+                                    triples[triple] += 1
+                                else:
+                                    triples[triple] = 1
+                        id_subj.append(word_id)
 
 
-                id_list.append(word_id)
+                    elif key == OBJ:
 
+                        tmp_obj[word_id] = word
+                        for id in id_subj:
+                            if tmp_subj[id]['head'] == tmp_obj[word_id]['head']:
+                                counter += 1
+                                verb_key = word['head']
+                                verb = str.lower(sentence[verb_key]['form'])
+                                subj = str.lower(tmp_subj[id]['form'])
+                                obj = str.lower(tmp_obj[word_id]['form'])
+                                triple = (subj, verb, obj)
+                                if triple in triples:
+                                    triples[triple] += 1
+                                else:
+                                    triples[triple] = 1
+                        id_obj.append(word_id)
 
-
-
-
+    sorted_triples = sorted(triples.items(), key=operator.itemgetter(1), reverse=True)
+    print('Number of triples in corpus: ' + str(counter))
+    print('Most frequent triples: ')
+    for i in range(0, 5):
+        try:
+            print(sorted_triples[i])
+        except:
+            print('No more triples')
+            break
 
 if __name__ == '__main__':
 
-    MULTILINGUAL = False
+    MULTILINGUAL = True
 
-    if not MULTILINGUAL:
+    if MULTILINGUAL:
+
+        SUBJ = 'nsubj'
+        OBJ = 'obj'
+
+        column_names_u = ['id', 'form', 'lemma', 'upostag', 'xpostag', 'feats', 'head', 'deprel', 'deps', 'misc']
+        files = get_files('dependencies/ud-treebanks-v2.2', 'train.conllu')
+        for train_file in files:
+            if train_file == 'dependencies/ud-treebanks-v2.2/UD_Swedish-Talbanken/sv_talbanken-ud-train.conllu'\
+                    or train_file == 'dependencies/ud-treebanks-v2.2/UD_English-ParTUT/en_partut-ud-train.conllu' \
+                    or train_file == 'dependencies/ud-treebanks-v2.2/UD_Danish-DDT/da_ddt-ud-train.conllu':
+                sentences = read_sentences(train_file)
+                formatted_corpus = split_rows(sentences, column_names_u)
+                # print(train_file, len(formatted_corpus))
+                # find_pairs(formatted_corpus)
+                find_triples(formatted_corpus)
+    else:
 
         SUBJ = 'SS'
         OBJ = 'OO'
 
         column_names_2006 = ['id', 'form', 'lemma', 'cpostag', 'postag', 'feats', 'head', 'deprel', 'phead', 'pdeprel']
-
-        train_file = 'hej/train.conll'
-        # train_file = 'test_x'
-        test_file = 'hej/test.conll'
+        train_file = 'train.conll'
+        test_file = 'test.conll'
 
         sentences = read_sentences(train_file)
         formatted_corpus = split_rows(sentences, column_names_2006)
-        #print(train_file, len(formatted_corpus))
-        #print(formatted_corpus[0])
+        print(train_file, len(formatted_corpus))
         find_pairs(formatted_corpus)
-        #find_tripples(formatted_corpus)
-
-    else:
-
-        SUBJ = 'nsubj'
-        OBJ = 'obj'
-
-        """
-        
-        
-        column_names_u = ['id', 'form', 'lemma', 'upostag', 'xpostag', 'feats', 'head', 'deprel', 'deps', 'misc']
-    
-        files = get_files('hej','train.conll')
-        for train_file in files:
-            sentences = read_sentences(train_file)
-            formatted_corpus = split_rows(sentences, column_names_u)
-            print(train_file, len(formatted_corpus))
-            print(formatted_corpus[0])
-            
-            find_pairs(formatted_corpus)
-        
-        """
-
+        find_triples(formatted_corpus)
